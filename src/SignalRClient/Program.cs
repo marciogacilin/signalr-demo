044259a27ac08c9e2bde3bb24a4db9886a54cd49
Marcio Gacilin <marciogacilin@hotmail.com>
2024-03-15 15:31:21 -0300

    Arquivos do projeto

---
+++ src/SignalRClient/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+
+const string URL = "https://localhost:7186/chat";
+var conn = new HubConnectionBuilder()
+    .WithUrl(URL)
+    .Build();
+
+conn.On<string>("ReceiveMessage", (message) =>
+{
+    Console.WriteLine(message);
+});
+
+try
+{
+    await conn.StartAsync();
+    Console.WriteLine("Connection started.");
+
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
